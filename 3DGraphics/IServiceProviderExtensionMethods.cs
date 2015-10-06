443d3073ab8228ad12737cbc83d9db03f389c68d
TakeAsh <take@TakeAsh.net>
2015-10-06 19:27:31 +0900

    add IServiceProviderExtensionMethods
    - public static T GetService<T>(this IServiceProvider serviceProvider)

---
+++ 3DGraphics/IServiceProviderExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3DGraphics {
+
+    static class IServiceProviderExtensionMethods {
+
+        public static T GetService<T>(this IServiceProvider serviceProvider)
+            where T : class {
+
+            return serviceProvider != null ?
+                serviceProvider.GetService(typeof(T)) as T :
+                null;
+        }
+    }
+}
