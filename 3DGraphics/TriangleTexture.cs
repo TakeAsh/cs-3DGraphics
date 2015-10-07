7e60ce8a92cf7f28ec4808560f2b4dba8d83d321
TakeAsh <take@TakeAsh.net>
2015-10-07 11:36:21 +0900

    TriangleTexture.Create(): blend colors based on its areas with weight

--- 3DGraphics/TriangleTexture.cs
+++ 3DGraphics/TriangleTexture.cs
@@ -13,23 +13,34 @@ namespace _3DGraphics {
     public static class TriangleTexture {
 
         public const int DefaultSize = 8;
+        private const double DefaultDpi = 96;
+        private const double C0Weight = 0.67;
 
         public static BitmapSource Create(Color c0, Color c1, Color c2, int size = DefaultSize) {
+            if (size <= 0) {
+                return null;
+            }
             var format = PixelFormats.Bgra32;
             var bytesPerPixel = (format.BitsPerPixel + 7) / 8;
             var rawStride = size * bytesPerPixel;
             var rawImage = new byte[rawStride * size];
+            var area = size * size / 2.0;
             for (var v = 0; v < size; ++v) {
                 for (var u = 0; u <= size - v; ++u) {
-                    var uv = u + v;
-                    var c = c1.Blend(c2, (double)u / uv).Blend(c0, (double)uv / size);
+                    var area1 = size * v / 2.0;
+                    var area2 = size * u / 2.0;
+                    var area0 = area - (area1 + area2);
+                    var areaW = area0 * C0Weight + area1 + area2;
+                    var r1 = area1 / areaW;
+                    var r2 = area2 / areaW;
+                    var c = c0.Blend(c1, r1, c2, r2);
                     rawImage[u * bytesPerPixel + 0 + v * rawStride] = c.B;
                     rawImage[u * bytesPerPixel + 1 + v * rawStride] = c.G;
                     rawImage[u * bytesPerPixel + 2 + v * rawStride] = c.R;
                     rawImage[u * bytesPerPixel + 3 + v * rawStride] = c.A;
                 }
             }
-            return BitmapSource.Create(size, size, 96, 96, format, null, rawImage, rawStride);
+            return BitmapSource.Create(size, size, DefaultDpi, DefaultDpi, format, null, rawImage, rawStride);
         }
     }
 
