9bdf913e40c03ad91d7efffefb7ff6d18ccc9187
TakeAsh <take@TakeAsh.net>
2015-10-07 11:29:15 +0900

    ColorExtensionMethods: change amount -> ratio1 in Blend()

--- 3DGraphics/ColorExtensionMethods.cs
+++ 3DGraphics/ColorExtensionMethods.cs
@@ -11,19 +11,21 @@ namespace _3DGraphics {
         /// <summary>
         /// Blends the specified colors together.
         /// </summary>
-        /// <param name="color">Color to blend onto the background color.</param>
-        /// <param name="backColor">Color to blend the other color onto.</param>
-        /// <param name="amount">How much of <paramref name="color"/> to keep, "on top of" <paramref name="backColor"/>.</param>
+        /// <param name="color0">Base color.</param>
+        /// <param name="color1">Color onto <paramref name="color0" />.</param>
+        /// <param name="ratio1">How much of <paramref name="color1" /> on top of <paramref name="color0" />.</param>
         /// <returns>The blended color.</returns>
         /// <remarks>
         /// [c# - Is there an easy way to blend two System.Drawing.Color values? - Stack Overflow](http://stackoverflow.com/questions/3722307/)
         /// </remarks>
-        public static Color Blend(this Color color, Color backColor, double amount) {
-            var r = (byte)((color.R * amount) + backColor.R * (1 - amount));
-            var g = (byte)((color.G * amount) + backColor.G * (1 - amount));
-            var b = (byte)((color.B * amount) + backColor.B * (1 - amount));
-            var a = (byte)((color.A * amount) + backColor.A * (1 - amount));
-            return Color.FromArgb(a, r, g, b);
+        public static Color Blend(this Color color0, Color color1, double ratio1) {
+            var ratio0 = 1 - ratio1;
+            return Color.FromArgb(
+                (byte)(color0.A * ratio0 + color1.A * ratio1),
+                (byte)(color0.R * ratio0 + color1.R * ratio1),
+                (byte)(color0.G * ratio0 + color1.G * ratio1),
+                (byte)(color0.B * ratio0 + color1.B * ratio1)
+            );
         }
     }
 }
