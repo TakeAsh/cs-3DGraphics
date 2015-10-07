db7906acef8549b480580ff3283788647f8e6e58
TakeAsh <take@TakeAsh.net>
2015-10-07 11:30:58 +0900

    ColorExtensionMethods: add Blend()
    - public static Color Blend(this Color color0, Color color1, double
    ratio1, Color color2, double ratio2)

--- 3DGraphics/ColorExtensionMethods.cs
+++ 3DGraphics/ColorExtensionMethods.cs
@@ -27,5 +27,15 @@ namespace _3DGraphics {
                 (byte)(color0.B * ratio0 + color1.B * ratio1)
             );
         }
+
+        public static Color Blend(this Color color0, Color color1, double ratio1, Color color2, double ratio2) {
+            var ratio0 = 1 - (ratio1 + ratio2);
+            return Color.FromArgb(
+                (byte)(color0.A * ratio0 + color1.A * ratio1 + color2.A * ratio2),
+                (byte)(color0.R * ratio0 + color1.R * ratio1 + color2.R * ratio2),
+                (byte)(color0.G * ratio0 + color1.G * ratio1 + color2.G * ratio2),
+                (byte)(color0.B * ratio0 + color1.B * ratio1 + color2.B * ratio2)
+            );
+        }
     }
 }
