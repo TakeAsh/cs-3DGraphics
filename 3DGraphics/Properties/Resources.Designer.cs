87fb0742c29e8c64f25fea8cd8b8c687f9e254be
TakeAsh <take@TakeAsh.net>
2015-10-09 02:50:42 +0900

    add resource "Mesh01.txt"

--- 3DGraphics/Properties/Resources.Designer.cs
+++ 3DGraphics/Properties/Resources.Designer.cs
@@ -4,17 +4,18 @@
 //     ランタイム バージョン:4.0.30319.34209
 //
 //     このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
-//     コードが再生成されるときに損失したりします
+//     コードが再生成されるときに損失したりします。
 // </auto-generated>
 //------------------------------------------------------------------------------
 
 namespace _3DGraphics.Properties {
-
-
+    using System;
+    
+    
     /// <summary>
     ///   ローカライズされた文字列などを検索するための、厳密に型指定されたリソース クラスです。
     /// </summary>
-    // このクラスは StronglyTypedResourceBuilder クラスによって ResGen
+    // このクラスは StronglyTypedResourceBuilder クラスが ResGen
     // または Visual Studio のようなツールを使用して自動生成されました。
     // メンバーを追加または削除するには、.ResX ファイルを編集して、/str オプションと共に
     // ResGen を実行し直すか、または VS プロジェクトをビルドし直します。
@@ -22,29 +23,29 @@ namespace _3DGraphics.Properties {
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
-
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
         internal Resources() {
         }
-
+        
         /// <summary>
-        ///   このクラスに使用される、キャッシュされた ResourceManager のインスタンスを返します。
+        ///   このクラスで使用されているキャッシュされた ResourceManager インスタンスを返します。
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
-                if ((resourceMan == null)) {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("_3DGraphics.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   厳密に型指定されたこのリソース クラスを使用して、すべての検索リソースに対し、
         ///   現在のスレッドの CurrentUICulture プロパティをオーバーライドします。
@@ -56,6 +57,26 @@ namespace _3DGraphics.Properties {
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   # ply
+        ///# format binary_little_endian 1.0
+        ///# comment Running Screened Poisson Reconstruction (Version 7.0)
+        ///# comment 	--in sphere_vert.txt
+        ///# comment 	--depth 3
+        ///# comment 	--out sphere_mesh.ply
+        ///# comment 	--verbose
+        ///# comment #             Tree set in:       0.0 (s),       0.8 (MB)
+        ///# comment #      Constraints set in:       0.2 (s),       0.9 (MB)
+        ///# comment # Linear system solved in:       0.2 (s),       1.1 (MB)
+        ///# comment #        Got triangles in:       0.1 (s),       3.4 (MB)
+        ///# comment #            [残りの文字列は切り詰められました]&quot;; に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string Mesh01 {
+            get {
+                return ResourceManager.GetString("Mesh01", resourceCulture);
             }
         }
     }
